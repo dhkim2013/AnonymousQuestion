0c2cffff38bfda29be5eef8df875b0808ac2b687
dhkim2013 <dhkim20157@gmail.com>
2017-09-21 12:13:36 +0900

    Develop login logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from accounts import views
 
 urlpatterns = [
     url(r'^register/$', views.Register.as_view()),
+    url(r'^login/$', views.Login.as_view()),
+    url(r'^logout/$', views.Logout.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import json
 
+from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -17,6 +18,7 @@ from accounts.models import User
 @method_decorator(csrf_exempt, name='dispatch')
 class Register(View):
     def post(self, request):
+        logout(request)
         username = request.POST.get('username')
         password = request.POST.get('password')
         try:
@@ -24,3 +26,20 @@ class Register(View):
             return HttpResponse(status=200)
         except:
             return HttpResponse(status=409)
+
+class Login(View):
+    def post(self, request):
+        logout(request)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return HttpResponse(status=400)
+        else:
+            login(request, user)
+            return HttpResponse(status=200)
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponse(status=200)
